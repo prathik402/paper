038b240150faf3a7e502c12594f15a00f27d7e3d
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-07-25 20:13:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,24 @@
-const Render = Matter.Render;
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-var dustbin;
-var ground;
-var paper;
-var engine;
-let img;
+
 function preload()
 {
 	
-  
 }
 
 function setup() {
 	createCanvas(800, 700);
-  var render = Render.create({ element: document.body, engine: engine, options: { width: 1200, height: 700, wireframes: false } });
 
-
-  
 
 	engine = Engine.create();
 	world = engine.world;
 
 	//Create the Bodies Here.
-    ground = new Ground(400,670,800,20);
-    dustbin = new Dustbin(650,670);
-    paper = new Paper(500,500,50);
 
-    
+
 	Engine.run(engine);
   
 }
@@ -38,23 +27,10 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  if(keyCode === UP_ARROW){
   
-  }
-  dustbin.display();
-  ground.display();
-  paper.display();
-
-jump();  
-drawSprites();
+  drawSprites();
  
 }
-function jump(){
-if(keyCode  === UP_ARROW){
-Matter.Body.applyForce(paper.body,paper.body.position,{x:0,y:200});
 
-}  
-
-}
 
 
